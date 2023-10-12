2707269353a0c3fc875bedc27dee4106ecaaa74c
nortonisedAdeyemi <nortonadeyemi@gmail.com>
2023-10-11 18:11:09 -0700

    function, nested loop

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
- * main - function
- * Return: 0
+ * print_alphabet - this is the function of the task
+ * Return: return 0
  */
 
-int main(void)
+void print_alphabet(void);
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
 
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{	
+_putchar(c);
+}
+_putchar('\n');
 }
